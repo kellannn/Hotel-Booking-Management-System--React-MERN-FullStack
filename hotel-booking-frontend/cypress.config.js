fc3a03ee34d6a6a1c34ba5c2ad71fbdd17588d47
hippolala <krisna.agung05@gmail.com>
2026-06-19 15:28:55 +0700

    nambahin flow buat joki cypress

--- hotel-booking-frontend/cypress.config.js
+++ hotel-booking-frontend/cypress.config.js
@@ -2,8 +2,15 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    supportFile: false, // <-- ini yang hilang; proyek tidak punya cypress/support/e2e.js
+    // 🛡️ Menonaktifkan pelindung CORS agar robot bisa masuk dan mengetik di dalam Iframe Stripe
+    chromeWebSecurity: false, 
+    
+    // 📁 Menonaktifkan file support karena proyek kelompok 16 tidak menggunakan cypress/support/e2e.js
+    supportFile: false, 
+    
+    // 🔍 Mengizinkan Cypress mendeteksi seluruh berkas pengujian di folder e2e
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    
     setupNodeEvents(on, config) {
       return config;
     },
